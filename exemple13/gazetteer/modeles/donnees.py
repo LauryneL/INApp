fab7895edcf2e628d8977c48da4c0def08a89476
Lauryne <lemosquet@hotmail.fr>
2019-03-30 09:32:10 +0100

    essai bdd : bug

--- exemple13/gazetteer/modeles/donnees.py
+++ exemple13/gazetteer/modeles/donnees.py
@@ -2,9 +2,28 @@ from .. app import db
 
 
 # On crée notre modèle
-class video(db.Model):
-    chanteur = db.Column(db.Text)
-    id = db.Column(db.Integer, unique=True, nullable=False, primary_key=True, autoincrement=True)
-    chanson = db.Column(db.Text)
-    url = db.Column(db.Text)
+class BardINAge(db.Model):
 
+    RNO = db.Column(db.Text, unique=True, nullable=False, primary_key=True)
+    Description_detaille = db.Column(db.Text)
+    Titre_chanson = db.Column(db.Text)
+    Titre_emission = db.Column(db.Text)
+    Nom_chaine = db.Column(db.Text)
+    date_emission = db.Column(db.Text)
+    Nom_artiste = db.Column(db.Text)
+    ID_wikidata_artiste = db.Column(db.Text)
+    Bnfid = db.Column(db.Text)
+    Url_chanson_wd = db.Column(db.Text)
+    Id_wd_chanson = db.Column(db.Text)
+    Genre_chanson_wd = db.Column(db.Text)
+    Date_sortie_chanson_wd = db.Column(db.Text)
+    url_wd_personne = db.Column(db.Text)
+    date_naissace = db.Column(db.Text)
+    image_wd_chanteur = db.Column(db.Text)
+    url_bnf_chanson = db.Column(db.Text)
+    Bnf_id_song = db.Column(db.Text)
+    Url_artiste_dbp = db.Column(db.Text)
+    Image_artiste_dbp = db.Column(db.Text)
+    Date_naissance_artiste_dbp = db.Column(db.Text)
+    Date_mort_artiste_dbp = db.Column(db.Text)
+    url_bnf_chanteur = db.Column(db.Text)
--- exemple13/gazetteer/routes.py
+++ exemple13/gazetteer/routes.py
@@ -2,20 +2,20 @@ from flask import render_template, request
 
 
 from .app import app
-from .modeles.donnees import video
+from .modeles.donnees import BardINAge
 
 
 @app.route("/")
 def accueil():
     # On a bien sûr aussi modifié le template pour refléter le changement
-    videos = video.query.all()
+    videos = BardINAge.query.all()
     return render_template("pages/accueil.html", nom="INApp", videos=videos)
 
 
-@app.route("/player/<int:id>")
-def player(id):
+@app.route("/player/<RNO>")
+def player(RNO):
     # On a bien sûr aussi modifié le template pour refléter le changement
-    unique_video = video.query.get(id)
+    unique_video = BardINAge.query.get(RNO)
     return render_template("pages/place.html", nom="INApp", video=unique_video)
 
 
@@ -30,7 +30,7 @@ def recherche():
     # On fait de même pour le titre de la page
     titre = "Recherche"
     if motclef:
-        resultats = video.query.filter(
-            video.chanson.like("%{}%".format(motclef))
+        resultats = BardINAge.query.filter(
+            BardINAge.Description_detaillee.like("%{}%".format(motclef))
         ).all()
     return render_template("pages/recherche.html", resultats=resultats, titre=titre)
